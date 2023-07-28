4cdbd2c195e9d460c750acc2994a649cb7f47c18
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-07-28 11:37:34 -0400

    chore: DRY Subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,59 +1,16 @@
 class SubscriptionsController < ApplicationController
-  # POST /customers/:customer_id/subscriptions
-  # swagger_controller :subscriptions, 'Subscriptions'
-
-  # POST /customers/:customer_id/subscriptions
-  # swagger_api :create do
-  #   summary 'Creates a new subscription'
-  #   notes 'This endpoint takes in a customer_id, tea_id, title, price, and frequency and creates a new subscription'
-  #   param :form, :customer_id, :integer, :required, 'Customer ID'
-  #   param :form, :tea_id, :integer, :required, 'Tea ID'
-  #   param :form, :title, :string, :required, 'Title'
-  #   param :form, :price, :float, :required, 'Price'
-  #   param :form, :frequency, :string, :required, 'Frequency'
-  #   response :created, 'Subscription created successfully'
-  #   response :unprocessable_entity, 'Customer or Tea does not exist'
-  # end
+  before_action :find_customer_and_tea, only: [:create]
+  before_action :find_subscription, only: [:cancel]
 
   def create
-    customer = Customer.find_by(id: params[:customer_id])
-    tea = Tea.find_by(id: params[:tea_id])
-
-    if customer.nil? || tea.nil?
-      render json: { errors: ['Customer or Tea does not exist'] }, status: :unprocessable_entity
-    else
-      subscription = Subscription.create!(subscription_params)
-      render json: { message: 'Subscription created successfully' }, status: :created
-    end
+    subscription = Subscription.create!(subscription_params)
+    render json: { message: 'Subscription created successfully' }, status: :created
   end
-
-  # PUT /subscriptions/cancel/:id
-  # swagger_api :cancel do
-  #   summary 'Cancels a subscription'
-  #   notes 'This endpoint takes in a subscription id and cancels the subscription'
-  #   param :path, :id, :integer, :required, 'Subscription ID'
-  #   repsonse :ok, 'Subscription cancelled successfully'
-  #   response  :unprocessable_entity, 'Subscription does not exist'
-  # end
-
+  
   def cancel
-    subscription = Subscription.find_by(id: params[:id], status: 'active')
-
-    if subscription
-      subscription.update!(status: 'cancelled')
-      render json: { message: 'Subscription cancelled successfully' }, status: :ok
-    else
-      render json: { errors: ['Subscription does not exist'] }, status: :unprocessable_entity
-    end
+    @subscription.update!(status: 'cancelled')
+    render json: { message: 'Subscription cancelled successfully' }, status: :ok
   end
-
-  #GET /customers/:customer_id/subscriptions
-  # swagger_api :index do
-  #   summary 'Returns all subscriptions for a customer'
-  #   notes 'This endpoint takes in a customer id and returns all subscriptions for that customer'
-  #   param :path, :customer_id, :integer, :required, 'Customer ID'
-  #   response :ok, 'Subscriptions returned successfully'
-  # end
 
   def index
     customer = Customer.find(params[:customer_id])
@@ -66,8 +23,8 @@ class SubscriptionsController < ApplicationController
 
   def subscription_params
     {
-      customer_id: Customer.find_by(id: params[:customer_id]).id,
-      tea_id: Tea.find_by(id: params[:tea_id]).id,
+      customer_id: @customer.id,
+      tea_id: @tea.id,
       title: params[:title],
       price: params[:price],
       status: "active",
@@ -75,9 +32,25 @@ class SubscriptionsController < ApplicationController
     }
   end
 
+  def find_customer_and_tea
+    @customer = Customer.find_by(id: params[:customer_id])
+    @tea = Tea.find_by(id: params[:tea_id])
+
+    render_not_found(['Customer or Tea does not exist']) if @customer.nil? || @tea.nil?
+  end
+
+  def find_subscription
+    @subscription = Subscription.find_by(id: params[:id], status: 'active')
+    render_not_found(['Subscription does not exist']) unless @subscription
+  end
+
   def serialize_subscriptions(subscriptions)
     subscriptions.map do |subscription|
       SubscriptionSerializer.new(subscription).as_json
     end
+  end
+
+  def render_not_found(errors)
+    render json: { errors: errors }, status: :unprocessable_entity
   end
 end
